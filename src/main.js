b9dca4932c8f0d78e6dede96407b391fedb22cc5
zaneris123 <zaneris123@gmail.com>
2023-05-03 14:50:10 +0100

    Added state persistance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { firebaseApp } from './utils/connections';
 
 import App from './App.vue';
@@ -12,9 +12,10 @@ import { IonicVue } from '@ionic/vue';
 
 const modules = [VueFireAuth()];
 const app = createApp(App);
-
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 app.use(IonicVue);
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(VueFire, { firebaseApp, modules });
 
