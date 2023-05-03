46a9476dd03d54f402a63e8d4217cde43e35b9cb
bobbynagra92 <bobbynagra@hotmail.co.uk>
2023-05-03 19:53:38 +0100

    Header Added

--- src/main.js
+++ src/main.js
@@ -6,9 +6,24 @@ import { firebaseApp } from './utils/connections';
 import App from './App.vue';
 import router from './router';
 
-import './assets/main.css';
 import { VueFire, VueFireAuth } from 'vuefire';
 import { IonicVue } from '@ionic/vue';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
 
 const modules = [VueFireAuth()];
 const app = createApp(App);
