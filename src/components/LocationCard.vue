5a7ce7b5870c6232d355d6a6ca77e97a771cb69f
bobbynagra92 <bobbynagra@hotmail.co.uk>
2023-05-04 21:37:48 +0100

    Component Styling and some re-structuring

---
+++ src/components/LocationCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { useLocation } from '../stores/location';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle } from '@ionic/vue';
+
+const locationStore = useLocation();
+</script>
+
+<template>
+  <ion-card>
+    <img src="https://ionicframework.com/docs/img/demos/card-media.png" />
+    <ion-card-header>
+      <ion-card-subtitle>Recommended by {{ locationStore.selectedLoc.posted_by }}</ion-card-subtitle>
+      <ion-card-title>{{ locationStore.selectedLoc.name }}</ion-card-title>
+    </ion-card-header>
+  </ion-card>
+</template>
+
+<style scoped>
+ion-card {
+  position: absolute;
+  width: 95vw;
+  margin-left: 2.5vw;
+  height: 85vh;
+  overflow: scroll;
+}
+
+ion-card * {
+  display: flex;
+  width: 99%;
+  max-width: 120vh;
+  justify-content: center;
+  align-items: center;
+  margin: 1% auto;
+}
+
+ion-card img {
+  margin: 1% auto;
+}
+
+</style>
