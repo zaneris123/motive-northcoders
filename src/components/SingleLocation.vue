d62e53ecc6740494a75cfad49cd926ceb6fc583a
zaneris123 <zaneris123@gmail.com>
2023-05-05 16:17:54 +0100

    Added img to individual

--- src/components/SingleLocation.vue
+++ src/components/SingleLocation.vue
@@ -3,26 +3,49 @@ import { getSingleLocation } from '../utils/getSingleLocation';
 import { useRoute } from 'vue-router';
 import { ref } from 'vue';
 import { IonTitle, IonLoading } from '@ionic/vue';
-
 const route = useRoute();
 const locoId = route.params.location_id;
 const locationInfo = ref(null)
 const isLoading = ref(true)
+const imgNum = ref(0)
 getSingleLocation(locoId).then((location)=>{
     locationInfo.value = location;
-    console.log(location)
+    console.log(location.image)
     isLoading.value = false;
 })
+const ImageClickHandler = () =>{
+    if(imgNum.value < locationInfo.value.image.length-1){
+        imgNum.value++
+    } else{
+        imgNum.value = 0;
+    }
+}
 
 </script>
 
 <template>
+    <ion-content>
     <ion-loading :isOpen="isLoading"></ion-loading>
-    <ion-title v-if="locationInfo">
+    <ion-card>
+    <ion-title class="ion-text-wrap" v-if="locationInfo">
         <h1>{{ locationInfo.name }}</h1>
-        <h2>{{ locationInfo.categories.map((image)=>{return image}) }}</h2>
-
+        <ion-list v-for="category in locationInfo.categories">
+            <ion-label>{{ category }}</ion-label>
+        </ion-list>
+        <button @click="ImageClickHandler">
+            <ion-img id="locationImage" :src="locationInfo.image[imgNum]"/>
+        </button>
     </ion-title>
+</ion-card>
+</ion-content>
 </template>
+<style>
+#locationImage{
+    display: flex;
+    justify-content: center;
+    height:30vh;
+    width: auto
+}
+</style>
 
 
